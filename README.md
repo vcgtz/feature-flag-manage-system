52cba672b7ac6c1356b7046e510f52b74e85a2b1
Vicente Gutiérrez <gtz.rvera@gmail.com>
2024-07-01 22:09:55 -0600

    add base methods as placeholders
    Signed-off-by: Vicente Gutiérrez <gtz.rvera@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Feature Flag 🚩 Manage System
+
+## How to run it?
+```bash
+fastapi dev app/main.py --reload
+```
--- app/main.py
+++ app/main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
 @app.get("/")
 async def main():
     return {"message": "Welcome to the Feature Flag Manage System"}
+
+
+@app.get("/feature-flags")
+async def get_feature_flags():
+    return {"message": "List of the available feature flags"}
+
+
+@app.get("/feature-flags/{slug}")
+async def get_feature_flag(slug: str):
+    return {"message": "Get one feature flag by the slug: " + slug}
+
+
+@app.post("/feature-flags")
+async def create_feature_flag():
+    return {"message": "Create a new feature flag"}
+
+
+@app.put("/feature-flags/{slug}")
+async def update_feature_flag(slug: str):
+    return {"message": "Update the existing feature flag: " + slug}
+
+
+@app.delete("/feature-flags/{slug}")
+async def delete_feature_flag(slug: str):
+    return {"message": "Delete existing feature flag: " + slug}
